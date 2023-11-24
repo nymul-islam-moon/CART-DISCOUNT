687721b1b3ffe94b338dfbaaed865f06d7b7ec5f
Nymul Islam Moon <nymulislam.webappick@gmail.com>
2023-11-24 16:58:23 +0600

    creat deactive class

---
+++ classes/Deactive/Deactive.php
@@ -0,0 +1,9 @@
+<?php
+
+class Deactive
+{
+    public static function deactivate() {
+        // Your deactivation code here
+        // This method will be called when the plugin is deactivated
+    }
+}
